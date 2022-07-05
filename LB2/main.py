f17f186747d0bb0a9c23d02a61d7a4544967abce
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2022-07-05 15:22:51 +0200

    Fünfter Commit

--- LB2/main.py
+++ LB2/main.py
@@ -1,5 +1,5 @@
 from http import server
-from turtle import width
+from turtle import color, width
 from xhtml2pdf import pisa
 from dotenv import dotenv_values
 from email.message import EmailMessage
@@ -16,14 +16,19 @@ response = requests.request("GET", url)
 
 
 Witz = json.loads(response.content)["value"]
-print(Witz)
 
 
 html= f'''
 <html>
+<head>
+<style>
+</style> </head>
 <body>
 <h1>Chuck Norris API</h1>
-<p>{Witz}</p>
+<p><b>{Witz}</b></p>
+<div class="Cloud-img">
+    <img src="https://i.ds.at/gHYq0A/rs:fill:750:0/plain/2020/03/09/chuckBreiteKLEINER1200pix.jpg" width="720" height="347"></a>
+</div>
 '''
 
 
@@ -35,8 +40,8 @@ pdf_file.close()
 dotenv = dotenv_values(".env")
 
 user = dotenv.get("Email_Sendfrom")
-passcodeGmail = "kvwjfstqoczbehso"
-to = "y.kuenzler@icloud.com"
+passcodeGmail = dotenv.get("Email_Passcode")
+to = "marzio@hispeed.ch"
 
 
 with yagmail.SMTP(user, passcodeGmail) as yag:
